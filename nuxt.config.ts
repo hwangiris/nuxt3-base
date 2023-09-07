fd5f59941ed63fe557651ab070eb26437693fbc2
Iris Huang <iris@digisalad.cool>
2023-09-07 10:20:54 +0800

    formatter config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     runtimeConfig: {
-      public: {
-        base_url: process.env.NUXT_PUBLIC_BASE_URL,
-        gtm_id: process.env.NUXT_PUBLIC_GTM_ID,
-        gtm_debug: process.env.NODE_ENV === 'production' ? false : true,
-      }
+        public: {
+            base_url: process.env.NUXT_PUBLIC_BASE_URL,
+            gtm_id: process.env.NUXT_PUBLIC_GTM_ID,
+            gtm_debug: process.env.NODE_ENV === 'production' ? false : true,
+        }
     },
     modules: [
-      'nuxt-schema-org',
-      'nuxt-simple-sitemap'
+        'nuxt-schema-org',
         'nuxt-simple-robots',
+        'nuxt-simple-sitemap'
     ],
     schemaOrg: {
-      host: process.env.NUXT_PUBLIC_BASE_URL,
+        host: process.env.NUXT_PUBLIC_BASE_URL,
     },
     robots: {
         sitemap: [
